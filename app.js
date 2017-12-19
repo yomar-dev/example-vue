50faf2894f036f9a6762fe3e11897d718a6ece61
Yomar Miranda <mirandayomar@gmail.com>
2017-12-19 16:32:42 -0500

    Hemos creado la definición de los tres componentes visuales en los que hemos dividido nuestra interfaz.

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
+Vue.component('game-header', {
+	template: '<h1>Video Games</h1>'
+})
+
+
+Vue.component('game-add', {
+	template: `
+		<div>
+			<input type="text" v-model="titleGame" />
+			<button @click="emitNewGame">Añadir</button>
+		</div>
+	`,
+
+	data: function () {
+		return {
+			titleGame: null
+		}
+	},
+	
+	methods: {
+		emitNewGame: function () {
+			if(this.titleGame){
+				this.$emit('new', { title: this.titleGame })
+				this.titleGame = null
+			}
+		}
+	}
+})
+
+
+Vue.component('game-list', {
+	props: ['games'],
+	template: `
+		<ol>
+			<game-item v-for="item in games" :game="item" :key="item.id"></game-list>
+		</ol>
+	`
+})
+
+
+Vue.component('game-item', {
+	props: ['game'],
+	template: '<li>{{ game.title }}</li>'
+})
+
 /**
  * Instancia de nuestra App
  */
