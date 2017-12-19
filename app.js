224accfbae0c131e60432f07f677e24e828b31f6
Yomar Miranda <mirandayomar@gmail.com>
2017-12-19 15:55:42 -0500

    Hemos creado el modelo y el metodo en la zona reservada para ellos, en este 'games' lo hemos creado dentro de 'data' y 'addNewGame' en 'methods'. NOTA: Todos los modelos que una instancia o componente defina internamente se tienen que incluir dentro de 'data' y todos los metodos dentro de 'methods'.

--- app.js
+++ app.js
@@ -9,5 +9,17 @@ const app = new Vue({
 			<game-add @new="addNewGame"></game-add>
 			<game-list v-bind:games="games"></game-list>
 		</div>
-	`
+	`,
+	data: {
+		games: [
+			{ title: 'ME: Andromeda' },
+			{ title: 'Fifa 2017' },
+			{ title: 'League of Legend' }
+		]
+	},
+	methods: {
+		addNewGame function (game) {
+			this.games.push(game);
+		}
+	}
 })
