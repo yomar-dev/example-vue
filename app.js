65c180fd2f83b6a7520f38c0033bf3040a75bfd9
Yomar Miranda <mirandayomar@gmail.com>
2017-12-19 15:21:44 -0500

    Creamos un atributo llamado 'template' que contiene una pequeña plantilla con el HTML de nuestra App.

--- app.js
+++ app.js
@@ -2,5 +2,12 @@
  * Instancia de nuestra App
  */
 const app = new Vue({
-	el: '#app'
+	el: '#app',
+	template: `
+		<div class="view">
+			<game-header></game-header>
+			<game-add @new="addNewGame"></game-add>
+			<game-list v-bind:games="games"></game-list>
+		</div>
+	`
 })
